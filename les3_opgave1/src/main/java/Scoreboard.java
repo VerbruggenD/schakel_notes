e85bfe073214a7f790f7c76d41f05990bde04cb7
D3admous3 <43110723+D3admous3@users.noreply.github.com>
2022-03-09 17:35:35 +0100

    les3_oef1

---
+++ les3_opgave1/src/main/java/Scoreboard.java
@@ -0,0 +1,6 @@
+public class Scorebord {
+    public void voegToe(String x, int huidigeScore) { }
+    public int getTotaleScore(String x) { }
+    public String getWinnaar() { }
+}
+
---
+++ les3_opgave1/src/main/java/Speler.java
@@ -0,0 +1,4 @@
+public class Speler {
+    public String getNaam() { }
+    public int getScore() { }
+}
